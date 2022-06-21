abdbf4a346e5be8a5ab706c94eed1584c417449c
Jonathan Rodrigues <jonathan.wiz@outlook.com>
2022-06-21 17:51:39 -0300

    Requirement six

--- src/components/PlanetCard.jsx
+++ src/components/PlanetCard.jsx
@@ -6,8 +6,8 @@ class PlanetCard extends Component {
     const { planetName, planetImage } = this.props;
     return (
       <div data-testid="planet-card" className="cards">
-        <p data-testid="planet-name">{planetName}</p>
         <img src={ planetImage } alt={ `Planeta ${planetName}` } />
+        <p data-testid="planet-name">{planetName}</p>
       </div>
     );
   }
--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 import Title from './Title';
+import planets from '../data/planets';
+import PlanetCard from './PlanetCard';
 
 class SolarSystem extends Component {
   render() {
+    const planetsInfo = planets
+      .map(({ name, image }) => (
+        <PlanetCard planetName={ name } planetImage={ image } key={ name } />
+      ));
     return (
       <div data-testid="solar-system" className="solar-system">
         <Title headline="Planetas" />
+        <ul>
+          <li>{ planetsInfo }</li>
+        </ul>
       </div>
     );
   }
